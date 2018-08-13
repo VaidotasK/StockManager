8423b6f97aa1945a6f84841329cc2f392a24b163
Vaidotas <vaidotas.karoblis@gmail.com>
2018-08-13 23:23:40 +0300

    Added MIME type constants in BookEntry class

--- app/src/main/java/com/example/android/stockmanager/data/BookContract.java
+++ app/src/main/java/com/example/android/stockmanager/data/BookContract.java
@@ -1,5 +1,6 @@
 package com.example.android.stockmanager.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -27,6 +28,18 @@ public final class BookContract {
          * The content URI to access the book's data in the provider
          */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
 
         /**
          * Name of table
