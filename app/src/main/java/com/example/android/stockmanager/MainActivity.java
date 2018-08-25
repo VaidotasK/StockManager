76244ccd9aa61cfc7b2bc10750be26c5b1ffc96c
Vaidotas <vaidotas.karoblis@gmail.com>
2018-08-25 22:41:55 +0300

    Added Dummy data for insertBook method. Added code for empty view. Added OnCreateLoader, OnLoadFinished and onLoaderReset methods

--- app/src/main/java/com/example/android/stockmanager/MainActivity.java
+++ app/src/main/java/com/example/android/stockmanager/MainActivity.java
@@ -6,18 +6,26 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
 import com.example.android.stockmanager.data.BookContract.BookEntry;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int BOOK_LOADER = 1;
+
+    private BookCursorAdapter cursorAdapter;
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
-
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //Add empty view to list view
+        listView = findViewById(R.id.list_view_books);
+        View emptyView = findViewById(R.id.empty_view);
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        listView.setEmptyView(emptyView);
+
+        cursorAdapter = new BookCursorAdapter(this, null);
+        listView.setAdapter(cursorAdapter);
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
+        getSupportLoaderManager().initLoader(BOOK_LOADER, null, this);
 
 
     }
@@ -45,47 +65,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.menu_main_add_dummy_data:
+                insertBook();
+                return true;
+            // Respond to a click on the "Delete all entries" menu option
+            case R.id.menu_main_delete_all_entries:
+                //TODO code for delete book
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    private void displayDatabaseInfo() {
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query. "*" value means, all
-        String[] projection = {
-                "*"};
-
-        // Perform a query on books table
-        Cursor cursor = getContentResolver().query(BookEntry.CONTENT_URI,
-                projection,      // Columns to return
-                null,     // Columns for the WHERE clause
-                null,  // Values for the WHERE clause
-                null);   // Sort order
-
-        ListView listView = findViewById(R.id.list_view_books);
-        BookCursorAdapter adapter = new BookCursorAdapter(this, cursor);
-        listView.setAdapter(adapter);
-    }
-
-    //TODO change insertBook to private method
-    public void insertBook() {
+    private void insertBook() {
         // Create a ContentValues object where column names are the keys,
         // and info about book is values.
         //Dummy data was used
         ContentValues values = new ContentValues();
-        values.put(BookEntry.COLUMN_BOOK_AUTHOR, "Author");
-        values.put(BookEntry.COLUMN_BOOK_PRODUCT_NAME, "Book");
+        values.put(BookEntry.COLUMN_BOOK_AUTHOR, "Vaidotas K.");
+        values.put(BookEntry.COLUMN_BOOK_PRODUCT_NAME, "THE Book");
         values.put(BookEntry.COLUMN_BOOK_PUBLICATION_YEAR, 2000);
-        values.put(BookEntry.COLUMN_BOOK_LANGUAGE, "Language");
-        values.put(BookEntry.COLUMN_BOOK_TYPE, 2);
-        values.put(BookEntry.COLUMN_BOOK_PRICE, 12);
-        values.put(BookEntry.COLUMN_BOOK_QUANTITY, 1);
+        values.put(BookEntry.COLUMN_BOOK_LANGUAGE, "Lithuanian");
+        values.put(BookEntry.COLUMN_BOOK_TYPE, 0);
+        values.put(BookEntry.COLUMN_BOOK_PRICE, 9);
+        values.put(BookEntry.COLUMN_BOOK_QUANTITY, 10);
         values.put(BookEntry.COLUMN_BOOK_AVAILABILITY, 1);
-        values.put(BookEntry.COLUMN_BOOK_SUPPLIER_NAME, "Supplier");
-        values.put(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE_NUMBER, 863838484);
+        values.put(BookEntry.COLUMN_BOOK_SUPPLIER_NAME, "Vaidotas");
+        values.put(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE_NUMBER, +37063838494L);
 
         Uri newUri = getContentResolver().insert(BookEntry.CONTENT_URI, values);
+
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query. "*" value means, all
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_BOOK_PRICE,
+                BookEntry.COLUMN_BOOK_PRODUCT_NAME,
+                BookEntry.COLUMN_BOOK_AUTHOR,
+                BookEntry.COLUMN_BOOK_PUBLICATION_YEAR,
+                BookEntry.COLUMN_BOOK_QUANTITY};
+        return new CursorLoader(this, BookEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
 
     }
 }
