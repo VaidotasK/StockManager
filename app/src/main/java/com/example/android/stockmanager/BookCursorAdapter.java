7d44514beaccdffa893a5bac3466deb4f65514ec
Vaidotas <vaidotas.karoblis@gmail.com>
2018-08-28 05:41:44 +0300

    Minor changes

--- app/src/main/java/com/example/android/stockmanager/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/stockmanager/BookCursorAdapter.java
@@ -1,76 +1,87 @@
 package com.example.android.stockmanager;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.stockmanager.data.BookContract.BookEntry;
 
+import java.util.Currency;
+import java.util.Locale;
 
-/**
- * {@link BookCursorAdapter} is an adapter for a list or grid view
- * that uses a {@link Cursor} of pet data as its data source. This adapter knows
- * how to create list items for each row of pet data in the {@link Cursor}.
- */
 public class BookCursorAdapter extends CursorAdapter {
 
-    /**
-     * Constructs a new {@link BookCursorAdapter}.
-     *
-     * @param context The context
-     * @param c       The cursor from which to get the data.
-     */
     public BookCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
     }
 
-    /**
-     * Makes a new blank list item view. No data is set (or bound) to the views yet.
-     *
-     * @param context app context
-     * @param cursor  The cursor from which to get the data. The cursor is already
-     *                moved to the correct position.
-     * @param parent  The parent to which the new view is attached to
-     * @return the newly created list item view.
-     */
+
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
-    /**
-     * This method binds the pet data (in the current row pointed to by cursor) to the given
-     * list item layout. For example, the name for the current pet can be set on the name TextView
-     * in the list item layout.
-     *
-     * @param view    Existing view, returned earlier by newView() method
-     * @param context app context
-     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
-     *                correct row.
-     */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-
+    public void bindView(View view, Context c, Cursor cursor) {
         TextView priceTextView = view.findViewById(R.id.list_view_price);
         TextView productNameTextView = view.findViewById(R.id.list_view_product_name);
         TextView authorTextView = view.findViewById(R.id.list_view_author);
         TextView yearTextView = view.findViewById(R.id.list_view_publishing_year);
-        TextView quantityTextView = view.findViewById(R.id.list_view_quantity);
+        final TextView quantityTextView = view.findViewById(R.id.list_view_quantity);
+        String currency = Currency.getInstance(Locale.GERMANY).getCurrencyCode();
+        final Context context = c;
 
+        final int id = cursor.getInt(cursor.getColumnIndex(BookEntry._ID));
         int price = cursor.getInt(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_PRICE));
         String productName = cursor.getString(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_PRODUCT_NAME));
         String author = cursor.getString(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_AUTHOR));
         int year = cursor.getInt(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_PUBLICATION_YEAR));
-        int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_QUANTITY));
+        final int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(BookEntry.COLUMN_BOOK_QUANTITY));
 
-        priceTextView.setText(String.valueOf(price));
+        priceTextView.setText(String.valueOf(price + " " + currency));
         productNameTextView.setText(productName);
         authorTextView.setText(author);
         yearTextView.setText(String.valueOf(year));
         quantityTextView.setText(String.valueOf(quantity));
+
+        Button saleButton = view.findViewById(R.id.list_view_sale_button);
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int bookQuantityLeft = quantity;
+                if (0 == bookQuantityLeft) {
+                    Toast.makeText(context, R.string.msg_out_of_stock, Toast.LENGTH_SHORT).show();
+                } else if (0 < bookQuantityLeft) {
+                    bookQuantityLeft--;
+                    String newQuantityString = Integer.toString(bookQuantityLeft);
+
+                    ContentValues values = new ContentValues();
+                    values.put(BookEntry.COLUMN_BOOK_QUANTITY, newQuantityString);
+
+                    if (0 == bookQuantityLeft) {
+                        values.put(BookEntry.COLUMN_BOOK_AVAILABILITY, BookEntry.AVAILABILITY_NOT_AVAILABLE);
+                    }
+
+                    Uri currentBookUri = ContentUris.withAppendedId(BookEntry.CONTENT_URI, id);
+                    int rowsAffected = context.getContentResolver().update(currentBookUri, values, null, null);
+
+                    if (rowsAffected != 0) {
+                        quantityTextView.setText(String.valueOf(bookQuantityLeft));
+                    } else {
+                        Toast.makeText(context, R.string.button_sale_error_msg, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
+
 }
